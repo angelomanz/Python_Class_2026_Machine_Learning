b3a0996fc8aa5e027ef09a8bdfe9dc3463b08ef7
angelomanz <angelo.manzanares@gmail.com>
2026-02-19 17:33:19 -0600

    Add files via upload

---
+++ countdown.py
@@ -0,0 +1,11 @@
+weeks_left =8
+days_per_week =7
+total_days = weeks_left * days_per_week
+total_hours =total_days * 24
+
+print('Ml Program Countdown!')
+print('Weeks left:' , weeks_left)
+print("Total days" , total_days)
+print('Total_hours:' , total_hours)
+print('Only' , total_days, 'days until ML program!')
+print('Thats just' , total_hours, 'hours away - you got this!')
