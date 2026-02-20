04e3baef5effdfa3d58c3c3ea3e48707a06a05be
angelomanz <angelo.manzanares@gmail.com>
2026-02-19 18:40:55 -0600

    Add files via upload

---
+++ name_formatter.py
@@ -0,0 +1,11 @@
+name1 ='john smith'
+name2 ='ALICE JOHNSON'
+name3 = 'bob JONES'
+
+name1 = name1.strip().title()
+name2 = name2.strip().title()
+name = name3.strip().title()
+
+print(name1)
+print(name2)
+print(name3)
